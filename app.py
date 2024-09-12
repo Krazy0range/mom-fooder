6d38ff9475e385603d8b48590ecb915d493c6529
Krazy0range <77130315+Krazy0range@users.noreply.github.com>
2024-09-11 21:00:24 -0500

    the start of the beginning

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headers = {"User-Agent": "Mozilla/5.0"}
+    request = requests.get("https://www.costco.com", headers=headers)
+    data = request.text
+    return render_template("index.html", data=data)

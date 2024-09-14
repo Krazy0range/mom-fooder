2068318da6beffc16643ba9c4c2df1987a69ac5a
Krazy0range <77130315+Krazy0range@users.noreply.github.com>
2024-09-14 14:08:00 -0500

    SCRAPE COSTCO
    successfully indexing costco's entire site
    
    ...
    
    every single time you try to load the page :skull: :skull: :skull:
    
    dw im working on an indexer lol

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-import requests
+
+from scraper import Scraper
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
 def index():
-    headers = {"User-Agent": "Mozilla/5.0"}
-    request = requests.get("https://www.costco.com", headers=headers)
-    data = request.text
-    return render_template("index.html", data=data)
+    scraper = Scraper()
+    scraper.scrape_costco()
+    return render_template("index.html", products=scraper.costco_scraps)
--- static/script.js
+++
@@ -1,3 +0,0 @@
-const request = new Request("https://www.costco.com/")
-fetch(request)
-    .then((response) => {console.log(response.json())});
